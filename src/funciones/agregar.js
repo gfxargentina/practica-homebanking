2b3907856716ab7dee49c20c8ef0843ed2fe67a0
Daniel <daniel.villalobo@gmail.com>
2019-11-18 10:22:43 -0300

    bajé el index de Luis y corregí links

--- src/funciones/agregar.js
+++ src/funciones/agregar.js
@@ -23,10 +23,30 @@ export function agregar() {
     
     var e = document.getElementById("miSelect")
     let servicio = e.options[e.selectedIndex].value;
+       
+    //esta seccion no me funciona
+    let tarjeta=false;
+    let prestamo=false;
+    let cuenta=true;
+
+    if(servicio=='tarjeta'){
+        console.log("en tarjeta")
+        tarjeta=true;
+        console.log(tarjeta)
+    }else{
+        if(servicio=='prestamo'){
+            prestamo=true;
+           
+        }
+    }
+    //hasta aquí no funciona. No logro cambiar el servicio elegido por el usuario
            
     // crear el objeto persona 
-    let personaItem = new Persona(dni, nombre, apellido, direccion, telefono, email, salario, contrasena);
-   
+    var personaItem = new Persona(dni, nombre, apellido, direccion, telefono, email, salario, contrasena);
+    personaItem.cuenta=cuenta;
+    personaItem.tarjeta=tarjeta;
+    personaItem.prestamo=prestamo;
+      
     // agregar el objeto persona dentro del arreglo 
     nombreListaPersonas.push(personaItem);
 
@@ -35,6 +55,7 @@ export function agregar() {
         localStorage.setItem("keybank", JSON.stringify(nombreListaPersonas));
         document.getElementById("agregarForm").reset(); // resetea el formulario
         alert('Se agregó con éxito a nuestra base de datos. Estará pendiente de aprobación');
+        
     }else{ 
         alert('No se dió de alta. Ingrese por Login por favor...');
         document.getElementById("agregarForm").reset(); // resetea el formulario
--- src/funciones/buscar.js
+++ src/funciones/buscar.js
@@ -1,24 +1,36 @@
 export function buscar() {
 
     let arreglo = JSON.parse(localStorage.getItem("keybank"));
+    //crea un array con los datos del localstorage
+
+    alert('en buscar')
 
     let dni1 = document.getElementById("dni").value;
-    //buscar en el arreglo el dni 
+    //trae el número de DNI que ingresó el usuario
 
     if (arreglo != null) {
+        //verifica que el localstorage no esté vacío
+
         let nombreListaPersonas1 = arreglo.filter(function (persona) {
             return persona.dni == dni1;
+            //si el arreglo no está vacío: crea un array "filtrando" los datos del localstorage cuyo DNI coincida con el que ingresó el Usuario
 
         })
         if (nombreListaPersonas1.length > 0) {
+            //si ese arreglo no está vacío significa que existe por lo menos un usuario con ese número de DNI
+            
             alert('DNI ya existente... no se dará de alta');
             return true;
+            //retorna true cuando encontró ese DNI cargado en el localstorage
+
         } else {
             return false;
+            //retorna false cuando no encontró ese DNI cargado en el localstorage 
         }
 
     } else {
         return false;
+        //retorna false porque el localstorage cuando no encontró ese DNI cargado en el localstorage
     }
 
 
